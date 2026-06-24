d9927a9090fbcc10306416a826d1d4da8865a041
Nguyễn Quốc Dương <88808137+nguyenduongit@users.noreply.github.com>
2026-06-24 14:58:20 +0700

    Update InputScreen.tsx

--- src/screens/InputTab/InputScreen.tsx
+++ src/screens/InputTab/InputScreen.tsx
@@ -47,6 +47,7 @@ const QUOTA_PERCENTAGE_OPTIONS = [
   { label: '80%', value: 80 },
   { label: '75%', value: 75 },
   { label: '70%', value: 70 },
+  { label: '65%', value: 65 },
   { label: '60%', value: 60 },
   { label: '55%', value: 55 }, 
   { label: '50%', value: 50 },
